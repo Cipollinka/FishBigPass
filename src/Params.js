d29e0db6c80e1f7e4975ed0539f3b00d7ca11633
o.podilko <opodilko122@gmail.com>
2025-01-30 17:20:46 +0200

    change target date

--- src/Params.js
+++ src/Params.js
@@ -2,6 +2,6 @@ export default class Params {
   static keyPush = 'a1fad974-ec70-4c0d-91e8-641bd07a9a54'; // ключ OneSignal
   static appID = '6740183954'; // ID додатку в сторі
   static keyApps = '7TiJRVRonFC7tnq8MJNRBk'; // ключ Appsflyer
-  static targetDate = '2025-01-01'; // дата закінчення відльожки
+  static targetDate = '2025-01-02'; // дата закінчення відльожки
   static bodyLin = 'https://astounding-sovereign-contentment.space/YCmK5JwP'; // тіло лінки з якою будемо працювати
 }
